553a7c810bc847c1f8b4294d18cd02b2a5825ba2
karlstedt020 <99504933+karlstedt020@users.noreply.github.com>
2022-02-20 17:06:27 +0300

    Add files via upload

---
+++ create_sorted1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int cmp(const int *a, const int *b) {
+    return *a - *b;
+}
+
+int main(void) {
+    freopen("input.txt", "w", stdout);
+    int n = 10;
+    // генерация случайного массива заданной длины, отсортированного по возрастанию
+    int *arr = malloc(sizeof(int) * n);
+    srand(time(NULL));
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand();
+    }
+    qsort(arr, n, sizeof(int), (int(*) (const void *, const void *)) cmp);
+    printf("%d\n", n);
+    for (int i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    free(arr);
+    return 0;
+}
