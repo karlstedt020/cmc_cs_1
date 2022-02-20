0fdcc051849f84cb698e8da24335a9c8c20f02be
karlstedt020 <99504933+karlstedt020@users.noreply.github.com>
2022-02-20 17:29:31 +0300

    Add files via upload

---
+++ create_rand.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    freopen("input.txt", "w", stdout);
+    int n = 100;
+    // генерация случайного массива заданной длины
+    long long int *arr = malloc(sizeof(long long int) * n);
+    srand(time(NULL));
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand();
+    }
+    printf("%d\n", n);
+    for (int i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    free(arr);
+    return 0;
+}
